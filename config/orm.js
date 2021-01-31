ba58557e71ce9e5a6daf5c8718df2c899e940471
Heath Ward <heathw24@outlook.com>
2021-01-31 18:17:24 -0500

    set up file structure and most files

---
+++ config/orm.js
@@ -0,0 +1,41 @@
+// Import connections
+var connection = require("../config/connection.js");
+
+var orm = {
+  selectAll: function(tableInput, cb) {
+      var queryString = "SELECT * FROM" + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+          if (err) {
+              throw err;
+          }
+          cb(result);
+      });
+  },
+
+  insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO" + table + " (" + cols.toString() + " )" + "VALUES (" + vals.toString() + ");";
+
+     console.log(queryString);
+     connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
+  }, 
+
+  updateOne: function( table, condition, cb) {
+      var queryString = "UPDATE " + table + " SET [devoured=true] WHERE " + condition;
+      console.log(queryString);
+
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+  }
+};
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VAlUES ("bacon and cheddar");
+INSERT INTO burgers (burger_name) VAlUES ("Mushroom and garlic mayo");
+INSERT INTO burgers (burger_name) VAlUES ("Fried onion and egg");
